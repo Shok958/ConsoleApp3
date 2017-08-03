82d7065c4f2545de0f2c7d08dba03cb5d092584e
Antonio <antonio958@hotmail.com>
2017-08-03 10:58:11 +0200

    Generic

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,6 +10,48 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            var ticket = new Ticket { Total = 1000 };
+            var repositoryTicket = new RepositoryTicket();
+            repositoryTicket.Add(ticket);
+        }
+    }
+
+   public  abstract class EntityBase {
+
+        public Guid Id { get; set; }
+        public static Guid CreateIdentifier() {
+            return new Guid();
+        }
+
+    }
+
+    public class Ticket : EntityBase
+    {
+        public Decimal Total { get; set; }
+    }
+
+    public class Person : EntityBase
+    {
+        public string Name { get; set; }
+    }
+
+    public class RepositoryTicket:RepositoryGeneric<Ticket> {
+        public override void Add(Ticket entity)
+        {
+            base.Add(entity);
+        }
+    }
+
+    public class RepositoryPerson:RepositoryGeneric<Person>
+    {
+       
+    }
+
+    public class RepositoryGeneric<T> where T : EntityBase
+    {
+        public virtual void Add(T entity) {
+
+            entity.Id = EntityBase.CreateIdentifier();
         }
     }
 }
